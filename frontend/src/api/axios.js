2d8200c6d55dd37c9b97e09d971c5493d07d6502
Jnbhuvan <jnbhuvan01@gmail.com>
2026-03-23 10:24:53 +0530

    debugged axios.js so to make it work both locally and remotly

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// export default api;
+const BASE_URL =
+  import.meta.env.VITE_API_URL || import.meta.env.production.VITE_API_URL;
+console.log(BASE_URL);
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: BASE_URL,
 });
 
 export default api;
