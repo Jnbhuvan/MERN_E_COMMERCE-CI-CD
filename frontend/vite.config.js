61c4b3f8b2f96ddaa69fec2db0da65767a754095
BHUVAN JN <jnbhuvan01@gmail.com>
2026-04-03 11:23:56 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    allowedHosts: [
+      "autoscaling-997473907.ap-south-1.elb.amazonaws.com"
+    ]
+  }
 });
