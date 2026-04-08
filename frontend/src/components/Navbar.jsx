165b5638a3017b700b2a168d4967ad3744a7bac2
Jnbhuvan <jnbhuvan01@gmail.com>
2026-04-08 11:20:51 +0530

    changed navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,7 +45,7 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between p-4 shadow bg-black text-white">
       <Link to="/" className="font-bold text-xl">
-        StoreFront - MERN CI/CD Pipeline Project
+        Bhuvan J N - MERN CI/CD Pipeline Project
       </Link>
 
       <div className="flex gap-4 items-center">
